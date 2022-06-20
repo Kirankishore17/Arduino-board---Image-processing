b488bc99735dbd64c05010ce2138021ea2daf3a2
Kirankishore <39197993+Kirankishore17@users.noreply.github.com>
2022-06-20 19:19:30 +0530

    Final commit

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,5 +1,6 @@
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(7, OUTPUT);
   // Setting baudrate to 9600
   Serial.begin(9600);
   digitalWrite(LED_BUILTIN, LOW);
@@ -8,7 +9,17 @@ void setup() {
 void loop() {
   if(Serial.available() > 0){
       String s = Serial.readString(); // for incoming serial data
-      Serial.println(s);
-      digitalWrite(LED_BUILTIN, HIGH);
+      s.trim();
+      if(s.equals("1")){
+        Serial.println("Face present: " + s);
+        digitalWrite(LED_BUILTIN, HIGH);
+        digitalWrite(7, HIGH);
+      }
+      if(s.equals("0")){
+        Serial.println("Face absent: " + s);
+        digitalWrite(LED_BUILTIN, LOW);
+        digitalWrite(7, HIGH);
+      }
   }
+  delay(2000);
 }
--- face-detection.py
+++ face-detection.py
@@ -1,7 +1,37 @@
 import serial
+import cv2
+import time
 
-with serial.Serial('com4', 9600, timeout=0.1) as arduinoSerial:
-    data = '12345'
-    print('pyserial version: ' + serial.__version__)
-    # Transmit data serially
-    arduinoSerial.write(data.encode('utf-8'))
+faceCascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+vid = cv2.VideoCapture(0)
+while True:
+    ret, img = vid.read()
+    img = cv2.flip(img, 1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,     
+        scaleFactor=1.2,
+        minNeighbors=5,     
+        minSize=(20, 20)
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        # Display the video with a box around the face
+        cv2.imshow('video',img)
+    with serial.Serial('COM4', 9600, timeout=1) as arduinoSerial:
+        # face present: 1
+        # face absent: 0
+        data = str(1 if len(faces) > 0 else 0)
+        print(f'Data: {data}') 
+        # Transmit data serially
+        arduinoSerial.write(bytes(data, 'utf-8'))
+        time.sleep(0.5)
+    # Exit condition
+    k = cv2.waitKey(30) & 0xff
+    if k == ord('q'): # press 'q' to quit
+        break
+
+vid.release()
+cv2.destroyAllWindows()
